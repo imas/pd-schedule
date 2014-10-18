b010cfc6aaad8d955bfeed966410e155c3fd44e7
treby <treby@atelier-nodoka.net>
2014-10-18 23:08:35 +0900

    Improve shown information.

--- crawl.rb
+++ crawl.rb
@@ -2,15 +2,16 @@ require 'mechanize'
 
 agent = Mechanize.new
 agent.get('http://idolmaster.jp/schedule/index.php')
-
 raw_page = agent.page
 raw_page.save('cal.html')
 
 table = raw_page.search('table.List')
 table.search('tr').each do |row|
   last_column = row.search('td').last
-  first_column = row.search('td').first
   next if last_column.children.first.name != 'a'
-  puts first_column.children.first.attributes['alt'].value if first_column.attributes['class'].value == 'day2'
-  puts last_column.children.text
+  puts '--'
+  puts row.search('td').first.search('img').first.attributes['alt'].value if row.search('td').first.attributes['class'].value == 'day2'
+  puts row.search('.time2').first.text
+  puts row.search('.performance2 img').first.attributes['alt']
+  puts "#{last_column.children.text} (#{last_column.children.first.attributes['href']})"
 end
