73eab1573b3ea6e0077b7c06bd01c40ae898aba3
treby <treby@atelier-nodoka.net>
2014-11-11 23:52:58 +0900

    Do combine.

---
+++ combine.rb
@@ -0,0 +1,14 @@
+require 'icalendar'
+
+calendar = Icalendar::Calendar.new
+Dir.glob('ics/[0-9]*.ics').sort.each do |icsname|
+  open(icsname) do |file|
+    Icalendar.parse(file, true).events.each do |evt|
+      calendar.add_event(evt)
+    end
+  end
+end
+
+open('ics/pd-schedule.ics', 'w') do |out|
+  out.puts calendar.to_ical
+end
